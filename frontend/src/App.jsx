57c449d83fed12775489dc5e7cb1cf25b602e624
deepthisway <contactdeep01@gmail.com>
2024-06-23 21:12:16 +0530

    Added little more styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { CreateTodo } from './Components/CreateTodo'
 import { Todos } from './Components/Todos'
 
@@ -12,10 +9,19 @@ function App() {
       const data = await res.json();
       setTodos(data);
     })
+  
+    const style1 ={
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '10vh',
+      backgroundColor: 'lightblue'
+      
+    }
 
   return (
     <div>
-     <h1>TODO app</h1>
+     <h1 style= {style1}>TODO app</h1>
      <CreateTodo/>
      <Todos todos={todos} ></Todos>
     </div>
